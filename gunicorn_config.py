e78f25e4a6a52472081bc9a04e744635997a49e3
Filipe Ximenes <filipeximenes@gmail.com>
2014-01-28 13:56:12 -0300

    testing conf

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,7 +5,7 @@
 # pythonpath = server_config.DJANGO_PROJECT_FOLDER
 
 command = '/home/ubuntu/venvs/virtualenv/bin/gunicorn'
-pythonpath = '/home/ubuntu/sites/pocket_groups'
+pythonpath = '/home/ubuntu/sites/pocket_groups/pocket_groups'
 bind = '127.0.0.1:8001'
 workers = 3
 user = 'ubuntu'
