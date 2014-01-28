5c602a7c94f6c7b2c8bf33effd2e7f3df53afd54
Filipe Ximenes <filipeximenes@gmail.com>
2014-01-28 15:49:01 -0300

    a

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 
 # command = '/home/ubuntu/venvs/virtualenv/bin/gunicorn'
-# pythonpath = '/home/ubuntu/sites/pocket_groups/pocket_groups'
+pythonpath = '/home/ubuntu/sites/pocket_groups/pocket_groups'
 bind = '127.0.0.1:8001'
 workers = 3
 user = 'ubuntu'
