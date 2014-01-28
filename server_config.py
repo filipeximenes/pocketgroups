5535eef85e538f61201c34c668014133dc631082
Filipe Ximenes <filipeximenes@gmail.com>
2014-01-28 17:26:50 -0300

    updating server name

--- server_config.py
+++ server_config.py
@@ -2,10 +2,10 @@
 PROJECT_NAME = 'pocket_groups'
 
 VIRTUALENV_FOLDER = '/home/ubuntu/venvs/virtualenv'
-DJANGO_PROJECT_FOLDER = '/home/ubuntu/sites/' + PROJECT_NAME
+DJANGO_PROJECT_FOLDER = '/home/ubuntu/sites/pocket_groups'
 
 GIT_REPO = 'git@bitbucket.org:filipeximenes/pocket_groups.git'
 HTTPS_GIT_REPO = 'https://filipeximenes@bitbucket.org/filipeximenes/pocket_groups.git'
 
-DJANGO_HOSTS = ['ec2-54-205-60-229.compute-1.amazonaws.com']
-PROVISION_DJANGO_HOSTS = ['ec2-54-205-60-229.compute-1.amazonaws.com']
+DJANGO_HOSTS = ['ec2-50-19-70-81.compute-1.amazonaws.com']
+PROVISION_DJANGO_HOSTS = ['ec2-50-19-70-81.compute-1.amazonaws.com']
