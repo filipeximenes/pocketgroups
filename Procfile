9b26becbab26a1d6998530bc43b7a90b10b8082a
Filipe Ximenes <filipeximenes@gmail.com>
2014-01-30 01:11:20 -0300

    fineshing configuration

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn pocket_groups.wsgi
-worker: celery -A pocket_groups worker -B -l info;
+web: python pip install -r requirements.txt; manage.py syncdb; python manage.py migrate; gunicorn pocket_groups.wsgi
+worker: celery -A pocket_groups worker -B -l info
--- pocket_groups/pocket_groups/settings.py
+++ pocket_groups/pocket_groups/settings.py
@@ -69,7 +69,7 @@ from dj_database_url import parse as db_url
 DATABASES = {
     'default': config(
         'DATABASE_URL', 
-        # default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         cast=db_url
     )
 }
@@ -86,6 +86,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Pocket
